a5bf7533cbfb1b876a3bf388bbfe1cbc7bb4c110
Gavin <gvnwltrs@gmail.com>
2018-12-02 19:55:58 -0500

    kernel magic

--- Machine Learning A-Z/Part 3 - Classification/Section 16 - Support Vector Machine (SVM)/svm.py
+++ Machine Learning A-Z/Part 3 - Classification/Section 16 - Support Vector Machine (SVM)/svm.py
@@ -22,7 +22,7 @@ X_test = sc.transform(X_test)
 
 # Fitting SVM to the Training set
 from sklearn.svm import SVC
-classifier = SVC(kernel = 'linear', random_state = 0)
+classifier = SVC(kernel = 'rbf', random_state = 0)
 classifier.fit(X_train, y_train)
 
 # Predicting the Test set results
